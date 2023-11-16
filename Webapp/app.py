4d6abbe59a6d9328f4fa4bb13b53c2e4eb4c0770
vip11017 <vip11017@gmail.com>
2023-11-15 22:43:21 -0800

    Added Latitude and Longitude Feature

--- Webapp/app.py
+++ Webapp/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
 import spacy
 from youtube_transcript_api import YouTubeTranscriptApi
+from geopy import geocoders
+import geocoder
+
 
 app = Flask(__name__)
 nlp = spacy.load('en_core_web_sm')
+
 def GPE_extract(text):
     tokens = nlp(text)
     token_list=[]
@@ -12,6 +16,18 @@ def GPE_extract(text):
         if token.text.strip() not in token_list:
            token_list += [token.text.strip()]
     return token_list
+
+
+def get_coordinates(token_lst):
+    coord_list = []
+    for i in token_lst:
+        location = geocoder.osm(i)
+        if location.ok:
+           latitude, longitude = location.latlng
+           coord_list += [{"Location": i, "Latitude": latitude, "Longitude": longitude}]
+        else:
+           print("Error: " + i)
+    return coord_list
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -22,7 +38,8 @@ def home():
         transcript_text = ""
         for line in transcript:
            transcript_text += " " + line['text'].replace("\n"," ")
-        print(GPE_extract(transcript_text))
+        location_list = GPE_extract(transcript_text)
+        print(get_coordinates(location_list))
 
 
 
