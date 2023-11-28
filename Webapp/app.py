c3c9fa157c8184adf03073e4234255877fef763b
YoungHu2023 <huyang2022@126.com>
2023-11-27 18:47:46 -0800

    deleted hotel scrapping

--- Webapp/app.py
+++ Webapp/app.py
@@ -8,6 +8,7 @@ import requests
 from playwright.sync_api import sync_playwright
 import datetime
 import pandas as pd
+import string
 
 app = Flask(__name__)
 nlp = spacy.load('en_core_web_sm')
@@ -19,7 +20,6 @@ def GPE_extract(text):
       if token.label_ == "GPE":
         if token.text.strip() not in token_list:
            token_list += [token.text.strip()]
-    print(token_list)
     return token_list
 
 
@@ -40,52 +40,66 @@ def plot_points(coord_list):
     else:
        map_center = [sum(p[1] for p in coord_list) / len(coord_list), sum(p[2] for p in coord_list) / len(coord_list)]
     mymap = folium.Map(location=map_center, zoom_start=4)
+    
     for coord in coord_list:
-      folium.Marker([coord[1], coord[2]], popup=coord[0]).add_to(mymap)
+        click_here = f' <a href="www.booking.com/searchresults.en-gb.html?ss={coord[0].replace(" ", "+")}" target="_blank">Go to Booking.com for a room! </a>'
+        booking_html = pd.DataFrame([{coord[0], f'Want to book a hotel at {coord[0]}?{click_here}'}]).to_html(escape=False, index=False)
+        print(click_here)
+
+        myframe = folium.IFrame(booking_html, width=800, height=100)
+        popup = folium.Popup(myframe, max_width=100)
+        folium.Marker([coord[1], coord[2]], popup=popup).add_to(mymap)
     return mymap
 
-def get_hotel(city):
+def get_hotel(city, browser):
+    
     with sync_playwright() as p:
-        
+        browser = p.chromium.launch(headless=False)
+    
         city = city.replace(" ", "+")
         
         # IMPORTANT: dates must be future dates, otherwise it won't work
-        checkin_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-        checkout_date = (datetime.date.today() + datetime.timedelta(days=2)).strftime("%Y-%m-%d")
+        checkin_date = (datetime.date.today() + datetime.timedelta(days=30)).strftime("%Y-%m-%d")
+        checkout_date = (datetime.date.today() + datetime.timedelta(days=31)).strftime("%Y-%m-%d")
         
-        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        hotels_list = []
+        hotel_list = []
 
-        page_num = 1
-        items_per_page = 0
+        page_url = f'https://www.booking.com/searchresults.en-us.html?checkin={checkin_date}&checkout={checkout_date}&selected_currency=USD&ss={city}&ssne={city}&ssne_untouched={city}&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure'.replace("{checkin_date}",checkin_date).replace("{checkout_date}",checkout_date).replace("{city}", city)
 
-        while (items_per_page >= 0):
-            offset = str((page_num - 1) * items_per_page)
-            page_url = f'https://www.booking.com/searchresults.en-us.html?checkin={checkin_date}&checkout={checkout_date}&selected_currency=USD&ss={city}&ssne={city}&ssne_untouched={city}&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure&offset={offset}'.replace("{checkin_date}",checkin_date).replace("{checkout_date}",checkout_date).replace("{city}", city).replace("{offset}", offset)
+        try:
+            page.goto(page_url, timeout=10000)
+        except:
+            print("get hotel page failed.")
+            return []
 
-            page.goto(page_url, timeout=100000)
+        hotels = page.locator('//div[@data-testid="property-card"]').all()
+        print(f'There are {len(hotel_list)} hotels.')
+    
+        count = 0
 
-            hotels = page.locator('//div[@data-testid="property-card"]').all()
-            print(f'There are {len(hotels)} hotels in page {page_num}.')
-            items_per_page = len(hotels) - 1
-            page_num += 1
-
-            for hotel in hotels:
-                hotel_dict = {}
+        for hotel in hotels:
+            if (count == 3):
+                break
+            hotel_dict = {}
+            try:
                 hotel_dict['hotel'] = hotel.locator('//div[@data-testid="title"]').inner_text()
                 hotel_dict['price'] = hotel.locator('//span[@data-testid="price-and-discounted-price"]').inner_text()
                 #hotel_dict['score'] = hotel.locator('//div[@data-testid="review-score"]/div[1]').inner_text()
                 #hotel_dict['avg review'] = hotel.locator('//div[@data-testid="review-score"]/div[2]/div[1]').inner_text()
                 #hotel_dict['reviews count'] = hotel.locator('//div[@data-testid="review-score"]/div[2]/div[2]').inner_text().split()[0]
 
-                hotels_list.append(hotel_dict)
-        
-        df = pd.DataFrame(hotels_list).drop_duplicates()
-        print(df)
-        
+                hotel_string = hotel_dict['hotel'].translate(str.maketrans("", "", string.punctuation)).replace("  "," ").replace(" ", "+")
+                hotel_dict['url'] = f'www.booking.com/searchresults.html?ss={hotel_string}'
+
+                hotel_list.append(hotel_dict)
+                count += 1
+            except:
+                print("get hotel failed.")
+
         browser.close()
-    return df
+
+    return hotel_list
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -93,30 +107,26 @@ def home():
 
 @app.route('/hotel', methods=['GET', 'POST'])
 def hotel():
+    if request.method == "POST":
+        url = request.form.get('youtube_url')
+        if ("youtube.com/watch" not in url) or ("This video isn't available anymore" in requests.get(url).text):
+            print("Invalid Link:" + url)
+            return render_template('error.html')
+    youtube_id = url.split("=")[1]       
+    transcript = YouTubeTranscriptApi.get_transcript(youtube_id)
+    transcript_text = ""
+    for line in transcript:
+        transcript_text += " " + line['text'].replace("\n"," ")
+    location_list = GPE_extract(transcript_text)
+    coord_list = (get_coordinates(location_list))
+    map = plot_points(coord_list)
+    map.save("templates/map.html")
+
     return render_template("hotel.html")
 
 @app.route('/map',methods=['GET', 'POST'])
 def map():
-    if request.method == 'POST':
-        url = request.form.get('youtube_url')
-        print("URL:" + url)
-        if ("youtube.com/watch" not in url) or ("This video isn't available anymore" in requests.get(url).text):
-            print("Invalid Link:" + url)
-            return render_template('error.html')
-        else:
-            youtube_id = url.split("=")[1]
-            transcript = YouTubeTranscriptApi.get_transcript(youtube_id)
-            transcript_text = ""
-            for line in transcript:
-                transcript_text += " " + line['text'].replace("\n"," ")
-            location_list = GPE_extract(transcript_text)
-            coord_list = (get_coordinates(location_list))
-            map = plot_points(coord_list)
-            map.save("templates/map.html")
     return render_template('map.html')
 
-
-   
 if __name__ == '__main__':
     app.run(debug=False)
-
